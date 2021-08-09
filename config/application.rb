29549c194491cbcbee36bbcb6c5887a2527fc21b
nfujita0707 <nfujita0714@gmail.com>
2021-08-09 15:02:08 +0000

    不要な記述やコメントは削除

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,7 @@ Bundler.require(*Rails.groups)
 
 module Blog0809
   class Application < Rails::Application
-    # config.load_defaults 5.2
-    config.time_zone = "Tokyo"
+        config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
   end
 end
